cac96a28318a5abe977e6c5a611a5c04931543a5
Holly Sandvold <holly.sandvold@gmail.com>
2022-11-18 20:23:35 -0500

    started coding

---
+++ 02_scripts/03_randomize_podcasts.py
@@ -0,0 +1,13 @@
+#a method to shuffle podcasts after the selected few is played 
+#uses the orginal list of fetched podcasts as well as new keywords from 
+#the selected podcasts
+#attempts to make the queue the length of the drive
+import json
+
+def main():
+    print("HI")
+
+    json.load
+
+if __name__ == '__main__':
+    main()
