f8ef895b399d9902ddabe98b7ff61051902129f1
Huzbah <48500154+Huzbah@users.noreply.github.com>
2022-11-19 16:16:41 -0500

    Retrieve podcasts done

--- 02_scripts/02_retrieve_podcasts.py
+++ 02_scripts/02_retrieve_podcasts.py
@@ -5,11 +5,11 @@ import requests
 from io import BytesIO
 from PIL import Image
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox,  QDateEdit,
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox,  QDateEdit, QPushButton,
     QMainWindow,
     QVBoxLayout,
-    QWidget,)
-from PyQt5.QtGui import QIcon
+    QWidget, QLabel)
+from PyQt6.QtGui import QIcon
 
 api_key = open("../01_data/private/key.txt", "r").readline() # load api
 
@@ -17,66 +17,121 @@ with open("../01_data/examplefile.txt", "rt") as f: #load genre ids selected
     for line in f:
         genre_ids= line.split(',')
         
+#declare variable to collect queue
+json_list=[] 
+queue_list=[]
+
+#fetch best podcasts in genre
+
+def get_podcast(term):
+    client = podcast_api.Client(api_key=api_key)      
+    response = client.fetch_best_podcasts(genre_id=term, page=1, region='us')
+    #print(response.json)
+    return response.json()
+
 #display best podcasts and select from list to prepare queue
 #merge json for recommendation during drive time
 
-json_list=[]
-#for term in genre_ids:
-response=get_podcast(genre_ids[0])
-    #display in gui
-
-json_list=response.get('podcasts')
-
-pod_dict={}
-pick=1 # allows to choose for 5 pods
-
-i=0
-#for each podcast store the title and image to add to combobox
-for pod in json_list:
-    i=i+1
-    pod_dict[pod.get('title')]=pod.get('image')
-    if i==pick:
-        break
+def get_title_dict(response):
+    pick=5 # allows to choose for 5 pods
+    i=0
+    pod_dict={}   #for each podcast store the title and image to add to combobox
     
-    
-import urllib.request
+    for pod in response:
+        i=i+1
+        pod_dict[pod.get('title')]=pod
+            #response = requests.get(pod.get('image'))
+            #img = Image.open(BytesIO(response.content))
+            #img.save("../03_output/"+pod.get('title')+".png", "PNG")
+        if i==pick:
+            break
+    return pod_dict
 
+#get podcasts with call to api
+response1=get_podcast(genre_ids[0]).get('podcasts')
+response2=get_podcast(genre_ids[1]).get('podcasts')
+response3=get_podcast(genre_ids[2]).get('podcasts')
+
+pod_dict1=get_title_dict(response1)
+pod_dict2=get_title_dict(response2)
+pod_dict3=get_title_dict(response3)
+
+#display option in gui
 # create a window to take in queue suggestions
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Create Queue")
+        
+        label = QLabel('Select podcasts for your queue:')
+        
         layout = QVBoxLayout()
     
-        combobox= QComboBox() # intitialize a combo box
+        combobox1= QComboBox() # intitialize a combo box
+        combobox2= QComboBox()
+        combobox3= QComboBox()
         
-        for pod in pod_dict:
-            urllib.request.urlretrieve(pod_dict[pod], "../03_output/"+pod)
-            #img = Image.open("../03_output/"+pod)
-            #img.save("../03_output/"+pod+".png", "PNG")
+        for pod in pod_dict1:
             
-            response = requests.get(pod_dict[pod])
-            img = Image.open(BytesIO(response.content))
-            img.save("../03_output/"+pod+".png", "PNG")
+            icon=QIcon("../03_output/icon.png")
+            combobox1.addItem(pod, icon)
+        
+         # Connect signals
+       
+        combobox1.currentTextChanged.connect(self.text_changed)
+        
+        for pod in pod_dict2:
             
-            icon=QIcon("../03_output/"+pod+".png")
-            combobox.addItem( pod)
+            icon=QIcon("../03_output/icon.png")
+            combobox2.addItem(pod, icon)
+        
+         # Connect signals
+       
+        combobox2.currentTextChanged.connect(self.text_changed)
+        
+        for pod in pod_dict3:
             
+            icon=QIcon("../03_output/icon.png")
+            combobox3.addItem(pod, icon)
+        
+         # Connect signals
+       
+        combobox3.currentTextChanged.connect(self.text_changed)
+        
+        confirm = QPushButton()
+        confirm.setText('Confirm Selection')
+        # confirm.setEnabled(False)
+        confirm.clicked.connect(self.close)
   
         layout = QVBoxLayout()
-        layout.addWidget(combobox)
+        layout.addWidget(label)
+        layout.addWidget(combobox1)
+        layout.addWidget(combobox2)
+        layout.addWidget(combobox3)
         
         container = QWidget()
         container.setLayout(layout)
+        layout.addWidget(confirm)
 
         self.setCentralWidget(container)
-
-    def current_text_changed(self, s):
-        print("Current text: ", s)
-
+        
+    def text_changed(self, s):
+        queue_list.append(s)
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
-
 app.exec()
+
+#print(queue_list)
+queue_dict={}
+
+queue_dict[queue_list[0]]=pod_dict1.get(queue_list[0])
+queue_dict[queue_list[1]]=pod_dict2.get(queue_list[1])
+queue_dict[queue_list[2]]=pod_dict3.get(queue_list[2])
+
+json_object = json.dumps(queue_dict)
+
+# Writing to file
+with open("../01_data/queue.json", "w") as outfile:
+    outfile.write(json_object)
