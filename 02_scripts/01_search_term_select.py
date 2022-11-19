e3bfb50f39e644be449ebd302fcf8c601fe70fa5
smikulin <somikulin@gmail.com>
2022-11-19 13:22:45 -0500

    buttons

--- 02_scripts/01_search_term_select.py
+++ 02_scripts/01_search_term_select.py
@@ -1,22 +1,65 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QLabel, QPushButton, QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout
+from PyQt6.QtGui import QIcon
 import sys
-from PyQt6 import QtWidgets, uic
-from MainWindow import Ui_MainWindow
-
-qtcreator_file = '/Users/sydneymikulin/Documents/CodeJamProject/03_output/mainwindow.ui' # Enter file here.
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
-
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.setWindowTitle('Podcast Mode')
-    window.show()
-    sys.exit(app.exec())
 
 genres = ['Personal Finance', 'Locally Focused', 'Sports', 'Business', 'Health & Fitness', 'Arts', 'Music', 'Technology', 'Fiction', 'History', 'Kids & Family', 'News', 'Comedy', 'Society & Culture', 'Religion & Spirituality', 'Government', 'TV & Film', 'Leisure', 'Education', 'Science', 'True Crime']
 genres.sort()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Podcast Mode')
+
+        label1 = QLabel('Select your first genre preference:')
+        self.genre1 = QComboBox()
+        self.genre1.addItem('Select First Genre')
+        self.genre1.addItems(genres)
+        self.genre1.activated.connect(self.check_index)
+
+
+        label2 = QLabel('Select your second genre preference:')
+        self.genre2 = QComboBox()
+        self.genre2.addItem('Select Second Genre')
+        self.genre2.addItems(genres)
+        self.genre2.activated.connect(self.check_index)
+
+        label3 = QLabel('Select your third genre preference:')
+        self.genre3 = QComboBox()
+        self.genre3.addItem('Select Third Genre')
+        self.genre3.addItems(genres)
+        self.genre3.activated.connect(self.check_index)
+
+        confirm = QPushButton()
+        confirm.setText('Confirm Selection')
+        # confirm.setEnabled(False)
+        confirm.clicked.connect(self.close)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label1)
+        layout.addWidget(self.genre1)
+        layout.addWidget(label2)
+        layout.addWidget(self.genre2)
+        layout.addWidget(label3)
+        layout.addWidget(self.genre3)
+        layout.addWidget(confirm)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def check_index(self, index):
+        cindex1 = self.genre1.currentIndex()
+        cindex2 = self.genre2.currentIndex()
+        cindex3 = self.genre3.currentIndex()
+        print(f"Index signal: {index}, currentIndex {cindex1}")
+
+    # def check_confirm(self, index):
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+sys.exit(app.exec())
