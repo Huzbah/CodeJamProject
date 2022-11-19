2a600bdcc3ce22244c6a2997c3dcc051e7eec55e
Holly Sandvold <holly.sandvold@gmail.com>
2022-11-19 14:48:43 -0500

    new api key

--- 02_scripts/03_randomize_podcasts.py
+++ 02_scripts/03_randomize_podcasts.py
@@ -3,11 +3,18 @@
 #the selected podcasts
 #attempts to make the queue the length of the drive
 import json
+import sys
 from listennotes import podcast_api
 from requests import get
-from playsound import playsound
+from pygame import mixer
 import random
 import multiprocessing
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox,  QDateEdit, QPushButton,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget, QLabel)
+from PyQt6.QtGui import QIcon
 
 
 def getJson(fileName): 
@@ -19,51 +26,32 @@ def getApiKey():
     f = open('./01_data/private/key.txt', 'r')
     return f.readline()
 
-def getRecommendation(episode_id):
+def getRecommendations(episode_id):
     api_key = getApiKey()
     client = podcast_api.Client(api_key=api_key)
     response = client.fetch_recommendations_for_episode(id=episode_id, safe_mode=0)
     
     return response
 
-def play(playlist):
-    i=0
-    while i < len(playlist):
-        audioLink = getEpisodeLink(playlist, i)
+def getPodcastTitle(episode):
+    try:
+        return episode["podcast"]["title"]
 
-        x = get(audioLink).content
-        file = open('./03_output/podcasts/podcast.mp3','wb')
-        file.write(x)
-        file.close()
-
-        p = multiprocessing.Process(target=playsound, args=('./03_output/podcasts/podcast.mp3',))
-        p.start()
-
-        #get name of the podcast
-        #stored differently sometimes, idk its the API
+    except:
         try:
-            epTitle = playlist[i]["title"]
-            podTitle = playlist[i]["podcast"]["title"]
+            return episode["podcast"]["title_original"]
         except:
-            try: 
-                epTitle = playlist[i]["title_orginal"]
-                podTitle = playlist[i]["podcast"]["title_original"]
-            except:
-                epTitle = ""
-                podTitle = ""
+            return ""
 
-        print("Currently listening to: "+ epTitle+" from "+ podTitle)
-        action = input("type SKIP to skip podcast or type STOP to end the ride \n")
-        if action == "SKIP":
-            p.terminate()
-        if action == "STOP":
-            p.terminate()
-            break
-        
-        messageNum = random.randint(1,2)
-        playsound('./03_output/podcasts/loadboardMessage'+str(messageNum)+'.mp3')
-        i=i+1
+def getEpisodeTitle(episode):
+    try:
+        return episode["title"]
 
+    except:
+        try:
+            return episode["title_original"]
+        except:
+            return ""
 
 def getEpisodeLink(playlist, i):
     if(i>=len(playlist)):
@@ -79,26 +67,109 @@ def removeDuplicates(playlist):
         if d["id"] not in seen:
             seen.append(d["id"])
             result.append(d)
+    return result
+
+def podcastPlayer(playlist):
+    return
 
     
-    #[result.append(x) for x in playlist if x not in result]
-    return result
+def playEpisode(playlist, i):  #Play
+    currentsong = playlist[i]
+    print(currentsong)
+    mixer.music.load(currentsong)
+    mixer.music.play()
+
+def pauseEpisode():
+    return
+    
+def play(playlist):
+    i=0
+    while i < len(playlist):
+        audioLink = getEpisodeLink(playlist, i)
+
+        x = get(audioLink).content
+        file = open('./03_output/podcasts/podcast1.mp3','wb')
+        file.write(x)
+        file.close()
+
+        #p = multiprocessing.Process(target=playsound, args=('./03_output/podcasts/podcast.mp3',))
+        #p.start()
+
+        #get name of the podcast
+        #stored differently sometimes, idk its the API
+        
+
+        print("Currently listening to: "+ epTitle+" from "+ podTitle)
+        action = input("type SKIP to skip podcast or type STOP to end the ride \n")
+        if action == "SKIP":
+            p.terminate()
+        if action == "STOP":
+            p.terminate()
+            break
+        
+        messageNum = random.randint(0,1)
+        playsound('./03_output/podcasts/loadboardMessage'+str(messageNum)+'.mp3')
+        i=i+1
+
+
+def stop():
+    return 0
+
+def popUp(p, episode):
+    # create a window to take in queue suggestions
+    class MainWindow(QMainWindow):
+        def __init__(self):
+            super().__init__()
+
+            self.setWindowTitle("Podcast Player")
+            
+            label = QLabel('Currently Playing:')
+            
+           # title = QLabel('Currently listening to: '+ epTitle+' from '+ podTitle)
+
+            pause = QPushButton()
+            pause.setText('Pause')
+            # confirm.setEnabled(False)
+            pause.clicked.connect(stop)
+    
+            layout = QVBoxLayout()
+            layout.addWidget(label)
+            layout.addWidget(title)
+            
+            container = QWidget()
+            container.setLayout(layout)
+            layout.addWidget(pause)
+
+            self.setCentralWidget(container)
+            
+        def text_changed(self, s):
+            return s
+
+        def current_text_changed(self, s):
+            return s
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+
+
+    app.exec()
 
 def main():
     # currently opening a test file
     selectedOptions = getJson('./01_data/example.json')
     
-    recommendations = []
-    #recommendations = getJson('./01_data/recommendations.json')
+    #recommendations = []
+    recommendations = getJson('./01_data/recommendations.json')
     playlist=[]
 
     for episode in selectedOptions["results"]:
         playlist.append(episode)
 
-    for list in selectedOptions["results"]:
-        for episode in getRecommendation(list["id"]).json()["recommendations"]:
-           recommendations.append(episode)
-  
+    #for chosenEpisode in selectedOptions["results"]:
+    #    for recommendedEpisode in getRecommendations(chosenEpisode["id"]).json()["recommendations"]:
+    #       recommendations.append(recommendedEpisode)
+    
     random.shuffle(recommendations)
     playlist=playlist+recommendations
     playlist=removeDuplicates(playlist)
--- 02_scripts/main.py
+++ 02_scripts/main.py
@@ -1,2 +1,3 @@
 import 01_search_term_select.py
 import 02_retreive_podcast.py 
+
