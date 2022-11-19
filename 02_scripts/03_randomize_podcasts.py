69f48a47d2c46dde588943f5736f455a85248c15
Holly Sandvold <holly.sandvold@gmail.com>
2022-11-19 13:56:32 -0500

    idk some random stuff

--- 02_scripts/03_randomize_podcasts.py
+++ 02_scripts/03_randomize_podcasts.py
@@ -9,13 +9,15 @@ from playsound import playsound
 import random
 import multiprocessing
 
+
 def getJson(fileName): 
     f = open(fileName)
     data = json.load(f)
     return data
 
 def getApiKey():
-    return '7591584d4b5d4dfd950f972ef7734277'
+    f = open('./01_data/private/key.txt', 'r')
+    return f.readline()
 
 def getRecommendation(episode_id):
     api_key = getApiKey()
@@ -24,10 +26,10 @@ def getRecommendation(episode_id):
     
     return response
 
-def play(recommendations):
+def play(playlist):
     i=0
-    while i < len(recommendations):
-        audioLink = getEpisodeLink(recommendations, i)
+    while i < len(playlist):
+        audioLink = getEpisodeLink(playlist, i)
 
         x = get(audioLink).content
         file = open('./03_output/podcasts/podcast.mp3','wb')
@@ -36,6 +38,21 @@ def play(recommendations):
 
         p = multiprocessing.Process(target=playsound, args=('./03_output/podcasts/podcast.mp3',))
         p.start()
+
+        #get name of the podcast
+        #stored differently sometimes, idk its the API
+        try:
+            epTitle = playlist[i]["title"]
+            podTitle = playlist[i]["podcast"]["title"]
+        except:
+            try: 
+                epTitle = playlist[i]["title_orginal"]
+                podTitle = playlist[i]["podcast"]["title_original"]
+            except:
+                epTitle = ""
+                podTitle = ""
+
+        print("Currently listening to: "+ epTitle+" from "+ podTitle)
         action = input("type SKIP to skip podcast or type STOP to end the ride \n")
         if action == "SKIP":
             p.terminate()
@@ -43,33 +60,53 @@ def play(recommendations):
             p.terminate()
             break
         
-        playsound('./03_output/podcasts/loadboardMessage.mp3')
+        messageNum = random.randint(1,2)
+        playsound('./03_output/podcasts/loadboardMessage'+str(messageNum)+'.mp3')
         i=i+1
 
 
-def getEpisodeLink(recommendations, i):
-    if(i>=len(recommendations)):
+def getEpisodeLink(playlist, i):
+    if(i>=len(playlist)):
         return "ERROR: out of episodes to play"
 
-    audioLink = recommendations[i]["audio"]
+    audioLink = playlist[i]["audio"]
     return audioLink
+
+def removeDuplicates(playlist):
+    result = []
+    seen = []
+    for d in playlist:
+        if d["id"] not in seen:
+            seen.append(d["id"])
+            result.append(d)
+
+    
+    #[result.append(x) for x in playlist if x not in result]
+    return result
 
 def main():
     # currently opening a test file
-    recommendations = getJson('./01_data/recommendations.json')
-
-    #for list in selectedOptions["results"]:
-    #    for episode in getRecommendation(list["id"]).json()["recommendations"]:
-    #        recommendations.append(episode)
+    selectedOptions = getJson('./01_data/example.json')
     
-    #audioLink = getRandomEpisodeLink(recommendations)
+    recommendations = []
+    #recommendations = getJson('./01_data/recommendations.json')
+    playlist=[]
+
+    for episode in selectedOptions["results"]:
+        playlist.append(episode)
+
+    for list in selectedOptions["results"]:
+        for episode in getRecommendation(list["id"]).json()["recommendations"]:
+           recommendations.append(episode)
+  
     random.shuffle(recommendations)
+    playlist=playlist+recommendations
+    playlist=removeDuplicates(playlist)
+    play(playlist)
 
-    play(recommendations)
 
-
-   #with open('./01_data/recommendations.json', "w") as outfile:
-    #    json.dump(recommendations,outfile, indent=4)
+    #with open('./01_data/recommendations.json', "w") as outfile:
+    #    json.dump(playlist,outfile, indent=4)
 
 if __name__ == '__main__':
     main()
