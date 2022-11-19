f5e23f0bd753d610e5c2da75cfb0b4c248eb98ac
smikulin <somikulin@gmail.com>
2022-11-19 10:29:43 -0500

    Update 01_search_term_select.py

---
+++ 02_scripts/01_search_term_select.py
@@ -0,0 +1,3 @@
+genres = ['Personal Finance', 'Locally Focused', 'Sports', 'Business', 'Health & Fitness', 'Arts', 'Music', 'Technology', 'Fiction', 'History', 'Kids & Family', 'News', 'Comedy', 'Society & Culture', 'Religion & Spirituality', 'Government', 'TV & Film', 'Leisure', 'Education', 'Science', 'True Crime']
+genres.sort()
+print(genres.count())
