9eec4f64b7c91b452ad4d50b354c3939ebc1eedb
Holly Sandvold <holly.sandvold@gmail.com>
2022-11-19 10:55:23 -0500

    boring updates

--- 02_scripts/03_randomize_podcasts.py
+++ 02_scripts/03_randomize_podcasts.py
@@ -6,6 +6,7 @@ import json
 from listennotes import podcast_api
 from requests import get
 from playsound import playsound
+import random
 
 def getJson(fileName): 
     f = open(fileName)
@@ -16,33 +17,40 @@ def getApiKey():
     return '7591584d4b5d4dfd950f972ef7734277'
 
 def getRecommendation(episode_id):
-
     api_key = getApiKey()
     client = podcast_api.Client(api_key=api_key)
     response = client.fetch_recommendations_for_episode(id=episode_id, safe_mode=0)
     
     return response
+
 def play(audioLink):
     x = get(audioLink).content
-    file = open('./03_output/podcast/podcast.mp3','wb')
+    file = open('./03_output/podcasts/podcast.mp3','wb')
     file.write(x)
     file.close()
 
-    playsound('podcast.mp3')
+    playsound('./03_output/podcasts/podcast.mp3')
+
+def getRandomEpisodeLink(recommendations):
+    randomNum = random.randint(0,len(recommendations))
+    audioLink = recommendations[randomNum]["audio"]
+    return audioLink
 
 def main():
     # currently opening a test file
     selectedOptions = getJson('./01_data/example.json')
-    recommendations = []
-    for list in selectedOptions["results"]:
-        for episode in getRecommendation(list["id"]).json()["recommendations"]:
-            recommendations.append(episode)
-    #audioLink = recommendations["recommendations"][0]["audio"]
-    #play(audioLink)
+    recommendations = getJson('./01_data/recommendations.json')
 
-    with open('./01_data/recommendations.json', "w") as outfile:
-        json.dump(recommendations,outfile)
-    #play(playNext)
+    #for list in selectedOptions["results"]:
+    #    for episode in getRecommendation(list["id"]).json()["recommendations"]:
+    #        recommendations.append(episode)
+    
+    audioLink = getRandomEpisodeLink(recommendations)
+    play(audioLink)
+
+
+   #with open('./01_data/recommendations.json', "w") as outfile:
+    #    json.dump(recommendations,outfile, indent=4)
 
 if __name__ == '__main__':
     main()
