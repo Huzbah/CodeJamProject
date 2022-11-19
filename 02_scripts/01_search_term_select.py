1210cc37864dbcd2a1c0131da634f8c521762ab0
smikulin <somikulin@gmail.com>
2022-11-19 11:21:49 -0500

    Made app

--- 02_scripts/01_search_term_select.py
+++ 02_scripts/01_search_term_select.py
@@ -1,3 +1,22 @@
+import sys
+from PyQt6 import QtWidgets, uic
+from MainWindow import Ui_MainWindow
+
+qtcreator_file = '/Users/sydneymikulin/Documents/CodeJamProject/03_output/mainwindow.ui' # Enter file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.setWindowTitle('Podcast Mode')
+    window.show()
+    sys.exit(app.exec())
+
 genres = ['Personal Finance', 'Locally Focused', 'Sports', 'Business', 'Health & Fitness', 'Arts', 'Music', 'Technology', 'Fiction', 'History', 'Kids & Family', 'News', 'Comedy', 'Society & Culture', 'Religion & Spirituality', 'Government', 'TV & Film', 'Leisure', 'Education', 'Science', 'True Crime']
 genres.sort()
-print(genres.count())
