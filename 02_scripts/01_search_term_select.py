cfa807d090e87443e4c82c8aeb04f57535d3917b
smikulin <somikulin@gmail.com>
2022-11-19 16:41:24 -0500

    Update 01_search_term_select.py

--- 02_scripts/01_search_term_select.py
+++ 02_scripts/01_search_term_select.py
@@ -3,8 +3,8 @@ from PyQt6.QtWidgets import QLabel, QPushButton, QComboBox, QMainWindow, QApplic
 from PyQt6.QtGui import QIcon
 import sys
 
-genres = ['Personal Finance', 'Locally Focused', 'Sports', 'Business', 'Health & Fitness', 'Arts', 'Music', 'Technology', 'Fiction', 'History', 'Kids & Family', 'News', 'Comedy', 'Society & Culture', 'Religion & Spirituality', 'Government', 'TV & Film', 'Leisure', 'Education', 'Science', 'True Crime']
-genres.sort()
+genres = {'Personal Finance':144, 'Locally Focused':151, 'Sports':77, 'Business':93, 'Health & Fitness':88, 'Arts':100, 'Music':134, 'Technology':127, 'Fiction':168, 'History':125, 'Kids & Family':132, 'News':99, 'Comedy':133, 'Society & Culture':122, 'Religion & Spirituality':69, 'Government':117, 'TV & Film':68, 'Leisure':82, 'Education':111, 'Science':107, 'True Crime':135}
+dict(sorted(genres.items()))
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,27 +15,27 @@ class MainWindow(QMainWindow):
         label1 = QLabel('Select your first genre preference:')
         self.genre1 = QComboBox()
         self.genre1.addItem('Select First Genre')
-        self.genre1.addItems(genres)
+        self.genre1.addItems(genres.keys())
         self.genre1.activated.connect(self.check_index)
 
 
         label2 = QLabel('Select your second genre preference:')
         self.genre2 = QComboBox()
         self.genre2.addItem('Select Second Genre')
-        self.genre2.addItems(genres)
+        self.genre2.addItems(genres.keys())
         self.genre2.activated.connect(self.check_index)
 
         label3 = QLabel('Select your third genre preference:')
         self.genre3 = QComboBox()
         self.genre3.addItem('Select Third Genre')
-        self.genre3.addItems(genres)
+        self.genre3.addItems(genres.keys())
         self.genre3.activated.connect(self.check_index)
 
         self.confirm = QPushButton()
         self.confirm.setText('Confirm Selection')
         self.confirm.setEnabled(False)
+        self.confirm.clicked.connect(self.make_file)
         self.confirm.clicked.connect(self.close)
-        self.confirm
 
         layout = QVBoxLayout()
         layout.addWidget(label1)
@@ -51,25 +51,28 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(container)
 
-    def check_index(self, index):
+    def check_index(self):
         cindex1 = self.genre1.currentIndex()
         cindex2 = self.genre2.currentIndex()
         cindex3 = self.genre3.currentIndex()
-        indices = cindex1, cindex2, cindex3
+        indices = [cindex1, cindex2, cindex3]
 
         if indices[0]>0 and indices[1] >0 and indices[2]>0:
             self.confirm.setEnabled(True)
         else:
              self.confirm.setEnabled(False)
 
-        return indices
-
-    # def allow_confirm(self, index):
-
-        
-
+    def make_file(self):
+        selection1 = self.genre1.currentText()
+        selection2 = self.genre2.currentText()
+        selection3 = self.genre3.currentText()
+        genre_selection = [genres.get(selection1), genres.get(selection2), genres.get(selection3)]
+        with open('../01_data/examplefile.txt', 'w') as f:
+            f.write(str(genre_selection))
 
 app = QApplication(sys.argv)
 w = MainWindow()
 w.show()
+
 sys.exit(app.exec())
+
